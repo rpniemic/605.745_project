004fa8604778b0c12f49bc092efd2017e68344a0
Brendan Byrne <brendan.c.byrne@gmail.com>
2015-03-02 00:04:52 -0500

    more on 5, still not working

--- homework/week_5/hw5.py
+++ homework/week_5/hw5.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python
 
+# over arching algorithm
+# http://bayes.cs.ucla.edu/BOOK-2K/d-sep.html
+
 import sys
 
 def main (filename):
     graph,start,finish,conditions = parse(filename)
-    
+        
     new_graph = apply_conditions(graph, conditions)
-    
-    route = find_route(new_graph, start, finish)
-    
-    if route is None:
-        print 'False'
-    else:
-        print 'True'
-
-def apply_conditions (graph, conditions):
-    return None
-
-def find_route (new_graph, start, finish):
-    return None
+        
+    return not path_exists(new_graph, start, finish)
 
 def parse (filename):
     with open(filename, 'rb') as f:
@@ -28,13 +20,58 @@ def parse (filename):
     str_nodes = raw[1].split(',')
     str_conditions = raw[2].split(',')
     
-    graph = [[int(s) for s in row] for row in str_graph]        
+    graph = [[int(s) for s in row] for row in str_graph]
     start = int(str_nodes[0])-1
     finish = int(str_nodes[1])-1
     conditions = [int(sc)-1 for sc in str_conditions]
 
     return graph, start, finish, conditions
         
+def apply_conditions (graph, conditions):
+    colliders = find_colliders(graph)
+    is_ok = [c in conditions for c in colliders]
+    
+    print 'conditions', conditions
+    print 'colliders', colliders
+    print 'is_ok', is_ok
+
+    #trimmed_graph = remove_collitions(graph, colliders, is_ok)
+    
+    return None
+
+def find_colliders (graph):
+    colliders = []
+    for i in xrange(len(graph)):
+        incoming = sum([row[i] for row in graph])
+        if incoming >= 2: colliders.append(i)
+    return colliders
+    
+def make_undirected (d_graph):
+    u_graph = [[value for value in row] for row in d_graph]
+    
+    for i,row in enumerate(d_graph):
+        for j, value in enumerate(row):
+            if value == 1: u_graph[j][i] = 1
+
+    return u_graph
+
+def path_exists (graph, start, finish):
+    # short circuited BFS to check for path existance
+    # https://en.wikipedia.org/wiki/Breadth-first_search#Pseudocode
+
+    queue = [start]
+    explored = [start]
+    while True:
+        if len(queue) == 0: return False
+        v = queue.pop(0)
+        
+        for w, val in enumerate(graph[v]):
+            if val == 0: continue
+            if w == finish: return True # short circuit
+            if w not in explored:
+                queue.append(w)
+                explored.append(w)
+
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         main(sys.argv[1])
